6c89cf102106f63d8e767b32014bf06a9e16def7
TristeMietitorr <108836582+TristeMietitorr@users.noreply.github.com>
2022-07-07 00:47:08 +0300

    tool
    tool

---
+++ allhackingtools.sh
@@ -0,0 +1,39 @@
+clear
+printf "================================
+ALL HACKING TOOL V0.1 
+================================\n\n"
+printf "by TristeMietitore" 
+printf "\n\n1-Userrecon"
+printf "\n2-Turkscout"
+printf "\n3-Zphisher"
+printf "\n4-MapEye"
+printf "\n5-About\n\n"
+
+menu () {
+
+read -p $'\n[*]Tool seçiminizi yapın: ' tools
+if [ $tools == 3 ]; then
+cd tools/zphisher
+bash zphisher.sh
+
+elif [ $tools == 5 ]; then
+cd tools/about
+bash about.sh
+
+elif [ $tools == 1 ]; then 
+cd tools/userrecon
+chmod +x userrecon.sh
+bash userrecon.sh
+
+elif [ $tools == 4 ]; then
+cd tools/MapEye 
+python3 mapeye.py
+python3 mapeye.py -t manual
+
+else
+printf "Böyle bir komut bulunmamaktadır."
+
+menu
+fi
+}
+menu
---
+++ tools/about/about.sh
@@ -0,0 +1,13 @@
+clear
+printf "================================
+ALL HACKING TOOL V0.1 ABOUT 
+================================\n\n"
+printf "by TristeMietitore" 
+printf "\n\nTool Hakkında 
+=================="
+printf "\nBu Tool TristeMietitore tarafından yapılmıştır.\n"
+printf "Toolun içindeki araçlar githubda güncel ve stabil çalışan araçları sizlere \nsunduk sizin de bildiğiniz varsa bana DM den yazarak tool önerebilirsiniz."
+printf "\n\nWARNING
+==============="
+printf "\n - Tool ile yaptığınız saldırılar ya da olumsuz sonuçların sorumluluğu bana ait değildir"
+printf "\nBol hackli günler dilerim :)"
